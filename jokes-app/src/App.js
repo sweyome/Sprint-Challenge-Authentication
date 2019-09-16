2e8fd970a71120029f35d92b9b4c0201472d512c
sweyome <sweyome1@gmail.com>
2019-09-16 19:50:22 -0400

    stretch update2

--- jokes-app/src/App.js
+++ jokes-app/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from "react-router-dom";
+
+// styles
 import './App.css';
+import Jokes from "./components/Jokes";
+
+// components
+import Login from "./components/Login"
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Route path="/jokes" component={Jokes}/>
+    <Login/>
+
     </div>
   );
 }
