c4f7066ae635f9d6774e326262100ac7c44eafbf
sweyome <sweyome1@gmail.com>
2019-09-15 20:14:43 -0400

    jokes model and auth router done

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,60 @@
 const router = require('express').Router();
+ const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('../jokes/jokes-model');
+const secrets = require('../config/secrets')  
+const usersModel = require('../jokes/jokes-model');
+
 
 router.post('/register', (req, res) => {
   // implement registration
-});
+   let user =req.body;
+  const hash = bcrypt.hashSync(user.password,10);
+  user.password = hash;
 
+  Users.add(user)
+  .then(saved => {
+      res.status(201).json(saved);
+  })
+  .catch(err => {
+      res.status(500).json(err)
+  });
+});
+ 
 router.post('/login', (req, res) => {
   // implement login
+   let {username, password} = req.body;
+  Users.findBy({username})
+  .first()
+  .then(user=> {
+    if(user && bcrypt.compareSync(password, user.password)){
+      const token = getToken (user)
+        //  req.session.user = user; 
+       res.status(200).json({
+        message: `Welcome${user.username}!`
+ });
+ 
+}else{
+  res.status(401).json({message: 'Invalid credentials'})
+}
+})
+.catch(err=> {
+res.status(500).json(err)
+})
 });
 
-module.exports = router;
+function getToken(user){
+  const payload ={
+    subject: user.id,
+    username: user.username,
+   };
+  const secret = secrets.jwtSecret;
+  const options = {
+    expiresIn: '1d'
+  };
+   
+
+ return jwt.sign(payload, secrets.jwtSecret, options);
+ 
+}
+module.exports =router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,8 +1,33 @@
+const bcrypt = require('bcryptjs');
+
+const Users = require('../jokes/jokes-model.js');
+
 /* 
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
 
+const secrets = require('../config/secrets.js');
+const jwt = require('jsonwebtoken');
+
+
+require('dotenv').config()
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+
+  if(req.headers.authorization.token){
+    try{
+      let decoded = jwt.verify(req.headers.token, secrets.jwtSecret);
+      console.log(decoded)
+      next();
+    }catch (err){
+      res.status(401).json({message: 'Invalid Token'})
+    }
+
+  }else{
+    res.status(401).json({message: 'Missing Token'});
+ 
+  }
 };
+
+
